7a14d455a314f03e8e48ec8a6df74ccb39391d17
Nilesh Yadav <yadavnilesh412001@gmail.com>
2021-09-26 20:07:35 +0530

    deleted Dummy component

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,10 +1,10 @@
-import Counter from "./Counter"
+
 
 export default function Summary() {
     return (
         <div>
             <h2>Summary</h2>
-            <Counter/>
+         
         </div>
     )
 }
